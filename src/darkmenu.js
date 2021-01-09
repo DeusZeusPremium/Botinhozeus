4b54822cc692121e148ac9dd3e27670d2fc2395a
DeusZeusPremium <77138942+DeusZeusPremium@users.noreply.github.com>
2021-01-09 11:49:29 -0300

    Add files via upload

---
+++ src/darkmenu.js
@@ -0,0 +1,36 @@
+const darkmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do BotZeus:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+}
+
+exports.darkmenu = darkmenu
+
+
+
+
+
+
+
+
+
